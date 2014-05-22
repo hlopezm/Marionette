bf21475674f59de6a3e61018d09fa7ad51e053d1
hlopez <hlopezm23@gmail.com>
2014-05-22 15:16:47 -0500

    Displaying our contacts with a composite View

--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -1,11 +1,15 @@
 ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbone, Marionette, $, _){
-  List.Contact = Marionette.ItemView.extend({
-    tagName: "li",
-    template: "#contact-list-item"
+List.Contact = Marionette.ItemView.extend({
+  tagName: "tr",
+  template: "#contact-list-item"
+});
+
+List.Contacts = Marionette.CompositeView.extend({
+    tagName: "table",
+    className: "table table-hover",
+    template: "#contact-list",
+    itemView: List.Contact,
+    itemViewContainer: "tbody"
   });
 
-  List.Contacts = Marionette.CollectionView.extend({
-    tagName: "ul",
-    itemView: List.Contact
-  });
 });
